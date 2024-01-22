c7baf6c278b4e1fbace96e2b440189f5e38a0ee6
Rey <rsantelli@luc.edu>
2024-01-22 00:52:29 -0600

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    const left = parseInt(dodger.style.left, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    const left = parseInt(dodger.style.left, 10);
+    
+    if (left < 360) { // assuming the container width is 400px
+        dodger.style.left = `${left + 1}px`;
+    }
+}
